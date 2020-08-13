bfbb64d11b4b87df9659fad842b3eb75a86c5b52
Tamerlan <omelchenkotamer@gmail.com>
2020-08-13 14:44:53 +0300

    Navigation buttons added.

--- src/main/java/com/tamerlan/blog/сontrollers/MainController.java
+++ src/main/java/com/tamerlan/blog/сontrollers/MainController.java
@@ -8,9 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-    public String greeting(Model model) {
-        model.addAttribute("title", "main title");
+    public String home(Model model) {
+        model.addAttribute("title", "\n" +
+                "Welcome to my page");
         return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "\n" +
+                "Let me tell you about myself");
+        return "about";
     }
 
 }
